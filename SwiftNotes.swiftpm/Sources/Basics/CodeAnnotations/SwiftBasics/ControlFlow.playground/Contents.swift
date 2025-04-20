0a2a79d0eea44c63a8bf7c7613b972abff6056dd
Liam Guedes <liam.guedes.cunha@gmail.com>
2025-04-20 13:49:27 -0300

    feat: [PRACTICAL] - Add Basic Control Flow and Boolean logic

--- SwiftNotes.swiftpm/Sources/Basics/CodeAnnotations/SwiftBasics/ControlFlow.playground/Contents.swift
+++ SwiftNotes.swiftpm/Sources/Basics/CodeAnnotations/SwiftBasics/ControlFlow.playground/Contents.swift
@@ -1,4 +1,4 @@
-// MARK: Boolean  Operator
+// MARK: Boolean Operator
 /*  Boolean operators in swift is used to compare values. It can be equality (==), different (!=), greater than (>) and less than (<)
 */
 
@@ -10,3 +10,25 @@ print(jamesAge != maryAge)
 print(jamesAge > maryAge)
 print(jamesAge < maryAge)
 print(jamesAge >= maryAge)
+
+// MARK: Boolean Logic
+/*  Boolean Logic */
+print(jamesAge > 20 && maryAge > 20) // AND
+print(jamesAge < 25 || maryAge < 25) // OR
+print(jamesAge == maryAge) // EQUAL
+print((jamesAge > 20 && maryAge > 20) || (jamesAge < 25 && maryAge < 25)) // AND OR AND
+
+// You can change true to false, using toggle method. But your variable, must be a VAR, if it be let, it doesn't work.
+var doorIsOpen = false
+doorIsOpen.toggle()
+
+// Control Flow - IF
+if doorIsOpen {
+    print("Door is open. Please close the door!")
+    doorIsOpen.toggle()
+}
+
+if !doorIsOpen && jamesAge > 20 {
+    print("You can enter in the PUB. Open the door")
+    doorIsOpen.toggle()
+}
