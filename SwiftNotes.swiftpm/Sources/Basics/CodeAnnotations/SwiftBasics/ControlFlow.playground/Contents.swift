0a94d1d7ad88e6e20c8bc64ee26e670d51023443
Liam Guedes <liam.guedes.cunha@gmail.com>
2025-04-22 00:48:18 -0300

    feat: [PRACTICAL] - Add For Loop and Switch statement

--- SwiftNotes.swiftpm/Sources/Basics/CodeAnnotations/SwiftBasics/ControlFlow.playground/Contents.swift
+++ SwiftNotes.swiftpm/Sources/Basics/CodeAnnotations/SwiftBasics/ControlFlow.playground/Contents.swift
@@ -62,3 +62,107 @@ while gas <= 100 {
     gas += 25
 }
 
+// Note: Variables is scope based, of you declare it inside a loop or if for example, this variable not exist outside, and if you try to put it in your code, it will generate a error.
+
+// When you'r working with ranges, you can add it into a variable
+let diceSides = 1...6
+while Int.random(in: diceSides) != 6 {
+    print("It's not a critical hit")
+}
+
+// For loop, will loop for a determined range. Different form while, this will increase a value time by time.
+let halfDayCounter = 1...12
+for hour in halfDayCounter {
+    print("Now is current \(hour):00")
+}
+
+// In the for loop, the variable is scoped and begins with let as default, but you can make it a var, like the example below.
+for var number in 1...6 {
+    number += 1
+}
+
+for number in 1...6 {
+    print("This is a number \(number)")
+}
+
+// Note: the continue will go to the next interaction and break will stop the loop
+
+for number in 1...10 {
+    if number % 2 == 0 {
+        print("Even number \(number)")
+        continue
+    }
+    
+    if number == 7 {
+        print("Joker Number")
+        break
+    }
+    
+    print("Odd number \(number)")
+}
+
+for row in  1...8 {
+    var line = ""
+    
+    for column in 1...8 {
+        if column % 2 == 0 {
+            line += "• "
+            continue
+        }
+        line += "◯ "
+    }
+    
+    print(line)
+}
+
+for rowX in  1...8 {
+    var line = ""
+    
+    for columnY in 1...8 {
+        if rowX == 1 || rowX == 8 {
+            line += "• "
+            continue
+        }
+        
+        if columnY == 1 || columnY == 8 {
+            line += "• "
+            continue
+        }
+        
+        line += "◯ "
+    }
+    
+    print(line)
+}
+
+// Switch is a control flow, that switch a value according the cases.
+let isACountry: String = "Brazil"
+
+switch isACountry {
+case "Brazil":
+    print("I love Brazil")
+case "France":
+    print("I love France")
+default:
+    print("I dont't know the name of this country")
+}
+
+let dayHour = 21
+switch dayHour {
+case 8...12:
+    print("Morning")
+case 13...18:
+    print("Afternoon")
+case 19...23:
+    print("Night")
+default:
+    print("Midnight")
+}
+
+let diceNumber = 4
+switch diceNumber {
+case let side where side % 2 == 0:
+    print("Even number")
+default:
+    print("Odd number")
+}
