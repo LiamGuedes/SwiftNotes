5b358d53246f12b96a11fc6ccfe3d31bb2e09826
Liam Guedes <liam.guedes.cunha@gmail.com>
2025-04-21 23:25:17 -0300

    feat: [PRACTICAL] - Add While Loop and Ternary Operator

--- SwiftNotes.swiftpm/Sources/Basics/CodeAnnotations/SwiftBasics/ControlFlow.playground/Contents.swift
+++ SwiftNotes.swiftpm/Sources/Basics/CodeAnnotations/SwiftBasics/ControlFlow.playground/Contents.swift
@@ -32,3 +32,33 @@ if !doorIsOpen && jamesAge > 20 {
     print("You can enter in the PUB. Open the door")
     doorIsOpen.toggle()
 }
+
+/*  Note, when you have two or more || or &&, the first statement always be run before, and if it become false,
+    the second statement will never be running. */
+
+if doorIsOpen && jamesAge < 30 {
+    print("Turn around, the VIP session is not avalable for you")
+} else if !doorIsOpen && jamesAge > 20 {
+    print("Go on. The PUB is open, but not VIP session")
+}
+
+// Ternary: (<CONDITION>) ? <TRUE VALUE> : <FALSE VALUE>
+// Note: You can run a closure or function in the ternary operator, not only value.
+let coupleOver18CanEnterThePub = jamesAge > 18 && maryAge > 18 ? "Can enter" : "Cann't enter"
+let coupleOver18CanEnterTheVIP = jamesAge > 30 && maryAge > 30 ? { doorIsOpen = true } : {doorIsOpen = false}
+
+// While Loop. This loop will run until a condition be satisfied. with repeat you condition will be read after the code, without repeat your condition will be read before.
+var gas = 100
+
+repeat {
+    print("Keep Running your car. Your gas is \(gas)LT")
+    gas -= 25
+} while gas > 0
+
+print("Your gas is empty. Please recharger it")
+
+while gas <= 100 {
+    print("Recharging your gas \(gas)LT")
+    gas += 25
+}
+
